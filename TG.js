782b53a9bde6fbbd8f18de920c03c9108e5e2fed
Peter Sønderby <31654763+Peter-Sonderby@users.noreply.github.com>
2018-11-14 15:18:05 +0100

    Favicon and more
    I have now updatet the project so it contains an favicon. at the same time, frontpage tamplete is made.

--- TG.js
+++ TG.js
@@ -1,4 +1,50 @@
 ﻿const TG = {
 
+    formTabel: (filelist) => {
+        var myObj, i, j, x = ""
+        console.log(filelist + " TG ");
+        var myObj = JSON.parse(filelist);
+        console.log(myObj + "  INFO lin 7");
+
+        for (i in myObj.info) {
+            Console.log(myObj.info[i].BookID)
+            $("#tabelStart").append(`
+              <tr>
+                <th class="cellHedder"> ${myObj.info[i].BookID}</th >
+                <th class="cellHedder"> ${myObj.info[i].IndexID}</th>
+                <th class="cellHedder"> ${myObj.info[i].FileName} </th>
+                <th class="cellHedder"> ${myObj.info[i].Supplier} </th>
+                <th class="cellHedder"> ${myObj.info[i].PageCount}</th>
+                <th class="cellHedder"> ${"25"} </th>
+                <th class="cellHedder"> ${myObj.info[i].Remarks} </th>
+              </tr>
+            `);
+        }
+        /*
+        info.forEach(info => {
+           
+            $("#tabelStart").append(`
+              <tr>
+                <th>${question.questionId}</th>
+                <th>${question.question}</th>
+                <th > </th>
+                <th class="cellHedder"> ${info.info.BookID}</th >
+                <th class="cellHedder"> ${info.IndexID}</th>
+                <th class="cellHedder"> ${info.info.FileName} </th>
+                <th class="cellHedder"> ${info.info.Supplier} </th>
+                <th class="cellHedder"> ${info.info.PageCount}</th>
+                <th class="cellHedder"> ${"25"} </th>
+                <th class="cellHedder"> ${info.info.Remarks} </th>
+              </tr>
+            `);
+
+        })
+
+        $("#tabelStart").append(`
+              <Button> add New file </button>
+            `);
+    */       
+    }
+    
 
 }
